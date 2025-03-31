57438e682c397e232150ca84f8992f2f3d659bba
Dimochka Diaz <anrok923@gmail.com>
2025-04-01 00:42:20 +0300

    .

--- ProjectX/ProjectX/ProjectX/Fill_Arr.cpp
+++ ProjectX/ProjectX/ProjectX/Fill_Arr.cpp
@@ -1,6 +1,7 @@
 #include "Fill_Arr.h"
 
 #include <iostream>
+#include <random>
 
 void FillArr(int32_t* arr, int32_t size) {
 	std::cout << "¬ведите все элементы массива >> ";
@@ -10,9 +11,21 @@ void FillArr(int32_t* arr, int32_t size) {
 }
 
 void RandFillArr(int32_t* arr, int32_t size) {
-	srand(time(NULL));
+	int32_t n{}, m{};
+	std::cout << "¬ведите диапозон элементов >> ";
+	std::cin >> n >> m;
+	if (n > m) {
+		int32_t temp{ n };
+		n = m;
+		m = temp;
+	}
+	
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(n, m);
+	
 	for (size_t i{}; i < size; ++i) {
-		arr[i] = { rand() % 100 - 50 };
+		arr[i] = { dist(gen) };
 	}
 }
 
